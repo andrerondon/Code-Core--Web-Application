df2b798a975780d7e2318afc4be5d82267518e48
andre-rondon <surplus@Andre-macbook-pro.local>
2020-02-05 15:18:08 -0800

    selection sort implementation

--- semester_2/7th-ap_sorting_and_searching/SelectionSort.js
+++ semester_2/7th-ap_sorting_and_searching/SelectionSort.js
@@ -17,4 +17,31 @@ function selectionSort(arr) {
         return arr;
     }
   
-  console.log(selectionSort([1, 23, 2, 56, 9, 8, 10, 100])); // [1, 2, 8, 9, 10, 23, 56, 100];
+  console.log(selectionSort([199, 2003, 2, 506, 900, 8, 990010, 100])); // [1, 2, 8, 9, 10, 23, 56, 100];
+
+
+  //////   brandon implementation
+
+//   function selectionSort(arr) {
+//     const sortedArr = [...arr];
+//     let subArrayIndex = 0;
+//     while(subArrayIndex < sortedArr.length) {
+//       let minValue = Infinity;
+//       let j = subArrayIndex;
+//       let minValueIndex;
+//       while(j < sortedArr.length) {
+//         if (sortedArr[j] < minValue) {
+//           minValue = sortedArr[j];
+//           minValueIndex = j;
+//         }
+//         j++
+//       }
+//       const currentZeroIndexValue = sortedArr[subArrayIndex];
+//       sortedArr[subArrayIndex] = minValue;
+//       sortedArr[minValueIndex] = currentZeroIndexValue;
+//       subArrayIndex++;
+//     }
+//     return sortedArr;
+//   }
+  
+//   console.log(selectionSort([15,42,1,67,104,2,2,2]));
