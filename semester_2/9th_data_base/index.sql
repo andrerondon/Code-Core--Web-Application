4d3da37c8c615a8c23ba670297e9abfdff3b32f9
andre-rondon <surplus@Andre-macbook-pro.local>
2020-02-12 10:45:47 -0800

    data base

--- semester_2/9th_data_base/index.sql
+++ semester_2/9th_data_base/index.sql
@@ -22,6 +22,53 @@ WHERE id > 100 AND id < 200
 ORDER BY id
 
 --Select all students whose ages are null or less than 20
-SELECT * 
+SELECT *
 FROM students
 WHERE age IS NULL OR age < 20
+
+--Find all students whose first names start with Jo (case sensitive)
+SELECT *
+FROM students
+WHERE first_name LIKE 'Jo%'
+
+
+-- same but insensitive
+SELECT *
+FROM students
+WHERE first_name iLIKE 'Jo%'
+
+--Find all students whose first names end with ‘on’ (case insensitive)
+SELECT *
+FROM students
+WHERE first_name iLIKE '%on'
+
+--Select all students whose first names or last names contain ‘nn’
+SELECT *
+FROM students
+WHERE first_name iLIKE '%nn%' OR last_name iLIKE '%nn%'
+
+--Select all students whose ages are between 25 and 35
+
+SELECT *
+FROM students
+WHERE age BETWEEN 25 and 35
+
+--Select all students that have registered between July 14, 2019 and July 28, 2019
+SELECT *
+FROM students
+WHERE registration_date BETWEEN 'July 14, 2019' AND 'July 28, 2019'
+
+--Select all students that whose first name is Daphnee, Norma, or Rene
+SELECT *
+FROM students
+WHERE first_name IN ('Daphnee', 'Norma', 'Rene')
+
+first_name = 'Daphnee'
+OR first_name = 'Norma'
+OR first = 'Rene'
+
+--Select all students whose age is one of 23, 26, or 29.
+
+SELECT *
+FROM students
+WHERE age IN (23, 26, 29)
