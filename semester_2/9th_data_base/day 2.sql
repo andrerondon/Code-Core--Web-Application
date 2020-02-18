da910f80f111f082efb477590d3dd590ea65de80
andre-rondon <surplus@Andre-macbook-pro.local>
2020-02-18 12:12:38 -0800

    sql

--- semester_2/9th_data_base/day 2.sql
+++ semester_2/9th_data_base/day 2.sql
@@ -25,6 +25,38 @@ SELECT * FROM students WHERE id = 7;
 
 Lab 1:  Find the Count of students whos first name starts with "L"
 --28
-SELECT count (last_name)
+SELECT COUNT(*)
 FROM students
-WHERE last_name iLIKE 'l%'
+WHERE first_name ILIKE 'L%'
+
+DEMO:  Get the number of students who are more than 25 years of age
+SELECT COUNT(*)
+FROM students
+WHERE age > 25
+
+Lab 2:  Find the sum of students ages whos first name starts with "L"
+
+
+Lab: Find out the average age of students who registered after March 1st 2018
+SELECT AVG(age)
+FROM students
+WHERE registration_date > 'March 1, 2018'
+
+Exercise: Write a query with the maximum, minimum, average and total age of all students
+SELECT MAX(age) AS max_age,
+MIN(age) AS min_age,
+ROUND(AVG(age), 2) AS average_age,
+SUM(age) AS total_age
+FROM students
+
+--COUNT and GROUP BY example
+SELECT first_name, count(*) AS NumberOfStudents
+FROM students
+GROUP BY first_name
+ORDER BY 2 DESC
+
+Exercise:  Find out the age that is the most common for all students.  Can return more than one age if needed
+SELECT age, count(*) AS NumberOfStudentsWithThatAge
+FROM students
+GROUP BY age
+ORDER BY 2 DESC
