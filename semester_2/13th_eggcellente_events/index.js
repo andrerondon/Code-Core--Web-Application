0f16c6489a965b9235e2b88f89879fa44cf25171
andre-rondon <rondon@estudiorondon.com.br>
2020-03-09 10:52:53 -0700

    add a router

--- semester_2/13th_eggcellente_events/index.js
+++ semester_2/13th_eggcellente_events/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express();
 
+// Routes
+
+// GET "/"
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
 const PORT = 3000
 const DOMAIN = 'localhost'
 app.listen(PORT, DOMAIN, () => {
