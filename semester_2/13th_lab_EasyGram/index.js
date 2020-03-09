ee128b6e1a63e0a9ed8b99c3599ca04056e7f423
andre-rondon <rondon@estudiorondon.com.br>
2020-03-09 15:40:07 -0700

    ease gram are groing

---
+++ semester_2/13th_lab_EasyGram/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const logger = require ('morgan')
+
+const app = express();
+
+app.set ('view engine', 'ejs')
+app.set ('views', 'views')
+
+app.use(logger('dev'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.render('welcome.ejs') 
+  })
+
+app.get('/users/new', (req, res) => {
+    console.log(req.query);
+    res.render('newUser.ejs') 
+  })
+
+app.post('/users/new', (req, res) => {
+    console.log(req.body)
+    res.send('thank you')   
+  })
+
+
+const PORT = 4200
+const DOMAIN = 'localhost'
+app.listen(PORT, DOMAIN, () => {
+  console.log(`Server Listening on ${DOMAIN}:${PORT}`)
+})
+
