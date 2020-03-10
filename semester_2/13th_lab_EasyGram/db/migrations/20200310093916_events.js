11338bb22b38b43792104a1e2de93278566286d4
andre-rondon <rondon@estudiorondon.com.br>
2020-03-10 09:46:55 -0700

    migration made well done

---
+++ semester_2/13th_lab_EasyGram/db/migrations/20200310093916_events.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('events', t => {
+      t.increments('id')
+      t.string('title')
+      t.text('description')
+      t.timestamps(true, true)
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('events')
+  };
+
+  //
