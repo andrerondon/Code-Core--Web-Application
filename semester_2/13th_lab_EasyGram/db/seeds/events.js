0d2644bb8bb2a82a54cf6911bc52a11e38daff0f
andre-rondon <rondon@estudiorondon.com.br>
2020-03-10 10:08:58 -0700

    install faker updat events seed

--- semester_2/13th_lab_EasyGram/db/seeds/events.js
+++ semester_2/13th_lab_EasyGram/db/seeds/events.js
@@ -1,11 +1,18 @@
+const faker = require('faker') // why is this faker?
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('events').del()
     .then(function () {
       // Inserts seed entries
-      return knex('events').insert([
-        {title: 'john snow', description: 'old fighter'}
-      ]);
+      // return knex('events').insert([
+      //   {title: 'john snow', description: 'old fighter'}
+      // ]);
+      return knex('events').insert(new Array(50).fill().map(() => {
+        return {
+          title: `${faker.hacker.ingverb()} ${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+          description: `${faker.hacker.phrase()}`
+        }
+      }))
     });
 };
