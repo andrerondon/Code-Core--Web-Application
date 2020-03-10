a3459f5f15b136182dc38a279bc14bda0fd30d6b
andre-rondon <rondon@estudiorondon.com.br>
2020-03-10 09:38:04 -0700

    database created

---
+++ semester_2/13th_lab_EasyGram/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',        // pg is the lubrary to connect client and the server
+    connection: {
+      database: 'eggcellente_events_wadd_1',
+      // username
+      // password      just for linux.... mac genious doesn't need it
+      
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+
+
+};
