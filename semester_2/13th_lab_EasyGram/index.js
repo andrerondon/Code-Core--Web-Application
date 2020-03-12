5c8ed62ac0c3f49789194a794e70067a327fd029
andre-rondon <rondon@estudiorondon.com.br>
2020-03-11 19:03:00 -0700

    Syntax fixed

--- semester_2/13th_lab_EasyGram/index.js
+++ semester_2/13th_lab_EasyGram/index.js
@@ -4,15 +4,15 @@ const usersRouter = require('./routes/users')
 const gramsRouter = require('./routes/grams')
 const welcomeRouter = require('./routes/welcomeRouter')
 
-const app = express();
+const app = express()
 
 app.set('view engine', 'ejs') // sets the "view engine" configuration to use 'ejs'. IE Telling ExpressJS to use EJS as our views
 app.set('views', 'views') // tell express our view files are in a directory called views
 
 app.use(logger('dev'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-////////Routers/////////////
+//  ////// Routers /////////////
 
 app.use('/users', usersRouter)
 app.use('/grams', gramsRouter)
@@ -21,8 +21,6 @@ app.use('/', welcomeRouter)
 // app.get('/', (req, res) => {
 //   res.render('welcome')
 // })
-
-
 
 const PORT = 4200
 const DOMAIN = 'localhost'
