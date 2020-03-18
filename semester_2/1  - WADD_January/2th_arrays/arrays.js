1d7f5a84208d466f4b46b395372bb7a0ac298f2d
andre-rondon <rondon@estudiorondon.com.br>
2020-03-18 08:46:26 -0700

    2th day with january team.. arrays

---
+++ semester_2/1  - WADD_January/2th_arrays/arrays.js
@@ -0,0 +1,11 @@
+// JS Arrays:
+// Arrays are an ordered collection of data
+
+// Use square brackets [] to define an array
+// Each element is separated by a comma
+[1, 2, 'a string', true, {}]
+
+// You can also nest arrays:
+[1, [2, [3, [4, [5, 6]]]]]
+[
+  1, [2, [3, [4, [5, 6]]]]]
