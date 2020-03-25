2feeffd24b6a441917d0ee9e8edc22a5b01d4a5e
andre-rondon <rondon@estudiorondon.com.br>
2020-03-25 13:37:26 -0700

    implrementing seed for users

--- semester_2/13th_eggcellente_events/db/seeds/users.js
+++ semester_2/13th_eggcellente_events/db/seeds/users.js
@@ -1,17 +1,28 @@
 const faker = require('faker')
+const cowsay = require('cowsay')
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert(new Array(5).fill().map(() => {
+      const usersArray = new Array(5).fill().map(() => {
         return {
           first_name: faker.name.firstName(),
           last_name: faker.name.lastName(),
           email: faker.internet.email(),
           password_digest: faker.internet.password()
         }
-      }))
+      })
+      // our test user
+      usersArray.unshift({
+        first_name: 'john',
+        last_name: 'snow',
+        email: 'js@gov.ca',
+        password_digest: 'supersecret'
+      })
+      console.log(cowsay.say({ text: 'Created 6 users' }))
+      // console.log(cowsay.say(JSON.stringify(usersArray[0])))
+      return knex('users').insert(usersArray)
     })
 }
