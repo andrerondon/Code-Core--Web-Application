4e88d51ea2ed75be873f156c25be980344705170
andre-rondon <rondon@estudiorondon.com.br>
2020-04-08 10:37:06 -0700

    startingruby course

---
+++ semester_2/15th_ruby_rails/ruby_demos.rb
@@ -0,0 +1,23 @@
+# To out put to the terminal like 'console.log' use print:
+print("Hello World \n")
+# print doesn't add a new line (i.e \n)
+
+puts("Goodbye World")
+# puts does add a new line (i.e \n)
+p("hello world")
+# Prints a value how ruby sees it. Useful for debugging 
+# Similar to 'console.dir'
+
+# This is a comment in Ruby
+
+=begin
+ This is a multiline 
+ comment 
+ in Ruby   
+=end
+
+# Most people
+# will just 
+# do this
+
+print "Hello World" # inline comment work too
