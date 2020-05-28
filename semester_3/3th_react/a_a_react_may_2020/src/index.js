d5475fd8933a5982751b00fe874b17400bfec579
andre-rondon <rondon@estudiorondon.com.br>
2020-05-28 11:58:26 -0700

    add components to react

--- semester_3/3th_react/a_a_react_may_2020/src/index.js
+++ semester_3/3th_react/a_a_react_may_2020/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
+// A react component is a function that returns a react element.
+// PascalCase is the naming convention for react components.
+// Components whose names does not begin with a capital letter,
+// will be interpreted as a plain HTML tag.
+
+const QuestionDetails = () => {
+  return (
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p>
+        Red, green, blue, magenta, etc. <br />
+        By Michael Owen
+      </p>
+      <p>
+        <small>Seen 10 times - Created 10 days ago</small>
+      </p>
+    </div>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <QuestionDetails />
   </React.StrictMode>,
-  document.getElementById("main-app")
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
