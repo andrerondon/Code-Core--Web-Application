6f4b04352b0133edbbc7dcd37f76d0f09ff98279
andre-rondon <rondon@estudiorondon.com.br>
2020-06-11 08:24:33 -0700

    wosh react app implements

--- semester_3/3th_react/mosh/counter-app/src/components/counter.jsx
+++ semester_3/3th_react/mosh/counter-app/src/components/counter.jsx
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
 class  Counter extends Component {
     state = {
-        count: 0,
+        count: 0
         // imageUrl: 'https://picsum.photos/200'
+        // tags : [ "tag1", "tag2", "tag3"]
     };
+
+    // constructor(){
+    //     super();
+    //     this.handleIncrement.bind = this.handleIncrement.bind(this);
+    // }
+    
+    // styles = {
+    //     fontSize: 20,
+    //     fontWeight: "bold"
+    // };
+    // renderTags() {
+    //     if (this.state.tags.length === 0)
+    //     return <p>There is no Tags!</p>;
+    //     return <ul>{this.state.tags.map(tag =>  <li>{ tag }</li>)}</ul>;
+    // }
+    handleIncrement = () =>  {
+        this.setState({ count: this.state.count + 1 })
+        
+    }
 
     render() {  
         return (
-        <div>
-            {/* <img src={this.state.imageUrl} alt=""/> */}
-            <span className="badge badge-primary m-2 ">{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm ">Increment</button>
-        </div>
+            <div>
+                {/* <img src={this.state.imageUrl} alt=""/> */}
+                <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={this.handleIncrement} 
+                    className="btn btn-secondary btn-sm "
+                >
+                    Increment
+                </button>
+                {/* {this.renderTags()} */}
+            </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
     }
 
     formatCount(){
         const { count } = this.state;
         return count === 0 ? "Zero" : count;
     }
-
-  
 }
- 
+
 export default Counter;
