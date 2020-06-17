e60b7ee2c21e0c791856a04872f70e73c5d925bf
andre-rondon <rondon@estudiorondon.com.br>
2020-06-16 19:31:47 -0700

    this was ed dev tutorial for react

--- semester_3/3th_react/react-dev-ed/src/App.js
+++ semester_3/3th_react/react-dev-ed/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Hello from './sayHello';
+import React, { useState } from 'react';
+import Tweet from './tweet'
+
 
 function App() {
-    
+    const [isRed, setRed] = useState(false)
+    const [count, setCount] = useState(0)
+    const increment = () => {
+        setCount (count + 1);
+        setRed(!isRed);
+    }
+
+    const [users, setUsers] = useState ([
+        { name: "Ed", message: "Hello There"},
+        { name: "André", message: "Hello There"},
+        { name: "Another", message: "Hello There"}
+    ])
     return  (
-        <div>
-            <Hello/>
+        <div className="app">
+            <h1 className= {isRed ? "red" : ""} > Change Color</h1>
+            <button onClick={increment}>Increment</button>
+            <h1> { count } </h1>
+            {users.map(user => (
+                <Tweet name={user.name} message={user.message} />
+            ))}
         </div>
+        
     );
 }
 
