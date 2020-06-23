44cd01cbb82e8f26f7858d4b4d3b4a6d4334b9dd
andre-rondon <rondon@estudiorondon.com.br>
2020-06-23 11:06:15 -0700

    finishing styling

--- semester_3/3th_react/react-review/src/components/Card.js
+++ semester_3/3th_react/react-review/src/components/Card.js
@@ -3,12 +3,24 @@ import React from 'react';
 function Card(props) {
   const character = props.character
   return(
-    <div key={character.id}>
+    <div key={character.id} style={styles.container}>
       <h2>{character.name}</h2>
       <img src='#' width='300px' height='300px'></img>
-      <p>{character.description}</p>
+      <p style={styles.text}>{character.description}</p>
     </div>
   )
+}
+
+const styles = {
+  container: {
+    width: '400px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  text: {
+    width: '300px'
+  }
 }
 
 export default Card
--- semester_3/3th_react/react-review/src/components/CharacterList.js
+++ semester_3/3th_react/react-review/src/components/CharacterList.js
@@ -4,10 +4,19 @@ import Card from './Card';
 function CharacterList(props) {
   const characters = props.characters;
   return(
-    <div>
+    <div style={style.container}>
       { characters.map(c => <Card key={c.id} character={c}/>) }
     </div>
   )
+}
+
+const style = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justfiyContent: 'space-evenly'
+  }
 }
 
 export default CharacterList
